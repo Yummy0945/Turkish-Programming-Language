e666618a2ff5c2b62b1d6dc7a8d76411166ae0f2
Yağmur <tokdemir.yummy@gmail.com>
2024-05-26 10:21:50 +0300

    Adding Project

---
+++ Turkish-Programming-Language-main/Source_Codes_and_Examples/Source_Codes/main.c
@@ -0,0 +1,43 @@
+
+#include<stdlib.h>
+#include<stdio.h>
+#include "parsing.h"
+#include "lexical.h"
+
+/*------------------------------------------*/
+//main fonksiyonu
+int main()
+{	
+	//TPL diliyle yazilmis kodu acar ve lexical analiz yapar
+	if(dosyaAc() == false)
+	{
+		printf("HATA : dosya bulunamadi.\n");
+	}
+	else 
+	{
+		getChar();
+		do 
+		{
+			lex();
+			if(nextToken == EOF && hataVarmi == false)
+			{
+				ekranaBas(94);
+			}
+			else if(hataVarmi == true)
+			{
+				ekranaBas(93);
+				ekranaBas(95);
+			}
+		} 
+		while(nextToken != EOF);
+				
+		if(hataVarmi == false)
+		{
+			dosyaAc();
+
+			program();
+		}
+	}
+	
+	return 0;
+}
